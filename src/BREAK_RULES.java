bf755e56397599fd1d490c76b2e9c24187926739
sEybers <167661614+sEybers@users.noreply.github.com>
2025-02-02 16:38:04 -0500

    THIS IS INTENDED TO CHECK IF CHECKSTYLE IS WORKING

--- src/BREAK_RULES.java
+++
@@ -1,9 +0,0 @@
-public class BREAK_RULES {
-    public static void main(String[] args){
-        BREAK_THE_RULES();
-    }
-    public static void BREAK_THE_RULES(){
-
-        System.out.println("We Love To Break The RULES!!!");
-    }
-}
---
+++ src/followRules.java
@@ -0,0 +1,10 @@
+public class followRules {
+    public static void main(String[] args){
+        followTheRules();
+    }
+    public static void followTheRules(){
+        System.out.println("We Like Rules");
+    }
+
+}
+
