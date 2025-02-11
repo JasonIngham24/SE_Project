3ba7b7fbcfe3724face632a0a05b5fc630eb1cde
jlicurse <jclicurse@outlook.com>
2025-02-10 21:08:30 -0500

    Implemented command-line argument input, updated processInput function in interface to accept an integer as the iteration in the fibonacci sequence (will update when local file input is implemented)

--- src/Prototype.java
+++ src/Prototype.java
@@ -7,16 +7,6 @@ public class Prototype implements UserComputeEngineAPI{
 		System.out.println("Output Location: " + dest); 
 	}
 	
-	public static void main(String[] args) {
-        Prototype engine = new Prototype();
-
-        // Example user input
-        String inputSource = "input_data.txt";  // The user specifies the source file
-        String delimiter = ",";                 // The user chooses comma as the delimiter
-        String outputDestination = "console";   // The user chooses console as the output destination
-
-        // Configure the job with the user's choices
-        engine.processInputData(inputSource, delimiter, outputDestination);
-    }
+	
 
 }
