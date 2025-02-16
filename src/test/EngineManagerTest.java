039debea7156088355ab5eb562ace86a4c5f5866
sEybers <167661614+sEybers@users.noreply.github.com>
2025-02-16 17:26:25 -0500

    Added EngineManagerTest

---
+++ src/test/EngineManagerTest.java
@@ -0,0 +1,32 @@
+package enginemangementapi;
+
+class EngineManagerTest implements EngineManagerAPI {
+
+	@Override
+	@Test
+	public void setData(int n) {
+		data = n;
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@Test
+	public int getData() {
+		// TODO Auto-generated method stub
+		if(data != null) {
+			return null;
+			//handles nullpointer exception
+		}
+		return 0;
+	}
+
+	@Override
+	@Test
+	public int sumOfNthEvenFibbonaciNums(int n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
