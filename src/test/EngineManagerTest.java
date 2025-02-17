f01a6d1a53e387b034bd7e26b81ff44aa2bc0515
sEybers <167661614+sEybers@users.noreply.github.com>
2025-02-16 20:20:07 -0500

    Commented Out Test File

--- src/test/EngineManagerTest.java
+++ src/test/EngineManagerTest.java
@@ -1,33 +1,33 @@
-package enginemangementapi;
-
-class EngineManagerTest implements EngineManagerAPI {
-
-	
-	@Test
-	public void setData(int n) {
-		int data = n;
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Test 
-	public int getData() {
-		// TODO Auto-generated method stub
-		int data = 1;
-		if(data != null) {
-			return null;
-			//handles nullpointer exception
-		}
-		return 1;
-	}
-
-
-	@Test 	
-	public int sumOfNthEvenFibbonaciNums(int n) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-
-}
+//package enginemangementapi;
+//
+//class EngineManagerTest implements EngineManagerAPI {
+//
+//	
+//	@Test
+//	public void setData(int n) {
+//		int data = n;
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//
+//	@Test 
+//	public int getData() {
+//		// TODO Auto-generated method stub
+//		int data = 1;
+//		if(data != null) {
+//			return null;
+//			//handles nullpointer exception
+//		}
+//		return 1;
+//	}
+//
+//
+//	@Test 	
+//	public int sumOfNthEvenFibbonaciNums(int n) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//	
+//
+//}
