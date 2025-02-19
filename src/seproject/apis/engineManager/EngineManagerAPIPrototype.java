794a657b7f10feb8d303b8eddc0cf278fcc2d002
sEybers <167661614+sEybers@users.noreply.github.com>
2025-02-19 10:16:42 -0500

    Seaneybers branch2 (#19)
    * Refactoring annotations/apis into folders
    
    * Forgot to stage a few changes when refactoring
    
    * corrected package issue so that all files are in the "seproject" package

---
+++ src/seproject/apis/engineManager/EngineManagerAPIPrototype.java
@@ -0,0 +1,29 @@
+//Prototype Class For EngineManagerAPI
+
+package seproject;
+public class EngineManagerAPIPrototype implements EngineManagerAPI {
+	
+	@Override
+	@ConceptualAPIPrototype
+	public void setData(int n) {
+		// TODO Auto-generated method stubs
+		
+	}
+
+	@Override
+	@ConceptualAPIPrototype
+	public int getData() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	@ConceptualAPIPrototype
+	public int sumOfNthEvenFibbonaciNums(int n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+}
