cc63a983d63b4856d44cfb55ecd9fc6c8ac67bc2
jlicurse <114960643+jlicurse@users.noreply.github.com>
2025-02-19 17:46:16 -0500

    Jclicurse branch (#24)
    Updated UserComputeEngineTest to fix NullPointerException

--- src/seproject/UserComputeEngineImpl.java
+++ src/seproject/UserComputeEngineImpl.java
@@ -5,17 +5,21 @@ public class UserComputeEngineImpl {
 	private SourceHandler sourceHandler; 
 	private StorageHandler storageHandler; 
 	
-	
+
 	public UserComputeEngineImpl(SourceHandler sourceHandler, StorageHandler storageHandler) {
 		this.sourceHandler = sourceHandler; 
 		this.storageHandler = storageHandler; 
 	}
 	
+
+
 	public SourceHandler sendInputSource(String source, String delimiter) {
 		System.out.print("sendInputSource called with source: " + source + "and delimiter: " + delimiter);
 		return sourceHandler; 
 	}
 	
+
+
 	public StorageHandler sendOutputDest(String dest) {
 		System.out.println("sendOutputDest called with dest: " + dest); 
 		return storageHandler; 
